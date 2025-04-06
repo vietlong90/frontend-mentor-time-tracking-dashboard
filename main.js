8dbb266d27a2b9720a5b2df1a1d9ff7c2f0b0738
vietlong90 <vietlong031290@gmail.com>
2025-04-06 11:11:42 +0700

    init

---
+++ main.js
@@ -0,0 +1,50 @@
+const data_time_tracking = [
+    {
+        "cat": "Work",
+        "style-class": "work",
+        "data": {
+            "daily": {
+                "current": 5,
+                "last_tracking": 7
+            },
+            "weekly": {
+                "time_tracking": 25,
+                "last_tracking": 35
+            },
+            "monthly": {
+                "time_tracking": 100,
+                "last_tracking": 150
+            }
+        }
+    },
+    {
+        "cat": "Play",
+        "time_tracking": 1,
+        "last_tracking": 2,
+        "style-class": "play"
+    },
+    {
+        "cat": "Study",
+        "time_tracking": 0,
+        "last_tracking": 1,
+        "style-class": "study"
+    },
+    {
+        "cat": "Exercise",
+        "time_tracking": 1,
+        "last_tracking": 1,
+        "style-class": "exercise"
+    },
+    {
+        "cat": "Social",
+        "time_tracking": 1,
+        "last_tracking": 3,
+        "style-class": "social"
+    },
+    {
+        "cat": "Self Care",
+        "time_tracking": 0,
+        "last_tracking": 1,
+        "style-class": "self-care"
+    }
+];
